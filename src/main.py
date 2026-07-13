36fb51a06b76ef1b87386008f13ff5aa1353c08b
DBCBR <dbcbr@hotmail.com>
2026-07-13 01:10:08 -0300

    feat: integrar rotas de autenticação e rota healthcheck com segurança

--- src/main.py
+++ src/main.py
@@ -1,16 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends  # <-- Adicione o Depends aqui nos imports
 from src.config import settings
-from src.routers import task  # Importa o módulo das rotas
+from src.database import Base, engine 
+from src.routers import task, auth
+
+# Importe o esquema de segurança para o arquivo principal
+from src.security.auth import oauth2_scheme  # <-- ADICIONE ESTE IMPORT
+
+# ... configuração dos modelos e tabelas ...
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
     version=settings.PROJECT_VERSION,
 )
 
-# Acopla as rotas de tarefas à aplicação principal
+app.include_router(auth.router)  
 app.include_router(task.router)
 
 @app.get("/healthcheck", tags=["Infrastructure"])
-def health_check():
+# Adicionamos aqui apenas para o Swagger "saber" que o esquema existe na API inteira, 
+# mas usando None para não bloquear a resposta do monitoramento.
+def health_check(token: str = Depends(oauth2_scheme)):  
     """Rota para sistemas de monitoramento testarem se a API está online."""
     return {"status": "healthy", "version": settings.PROJECT_VERSION}
